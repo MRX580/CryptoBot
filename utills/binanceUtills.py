2c22ea09c931ca4312e6d4d55f6db932f4251d5a
MRX580 <andrejchenko0511@mail.ru>
2022-08-23 11:34:50 +0200

    Update

--- utills/binanceUtills.py
+++ utills/binanceUtills.py
@@ -1,14 +1,15 @@
 from telegram_bot.database.sqlite import spot_database
 import math
 from binance.client import Client
-from config import API_KEY, SECRET_KEY
-
+from dotenv import find_dotenv, load_dotenv
+import os
+load_dotenv(find_dotenv())
 class BinanceClient:
-    _client = Client(API_KEY, SECRET_KEY)
+    __client = Client(os.getenv('API_KEY'), os.getenv('Secret_key'))
 
     def __init__(self, coin, profit, tf, usdt):
         self.coin = coin.upper()
-        self.priceCoin = float(self._client.get_symbol_ticker(symbol=self.coin)['price'])
+        self.priceCoin = float(self.__client.get_symbol_ticker(symbol=self.coin)['price'])
         self.profit = profit
         self.tf = tf
         self.usdt = usdt
@@ -23,25 +24,25 @@ class BinanceClient:
             '1d': '1440 hours ago UTC',
         }
         temp = []
-        klines = self._client.get_historical_klines_generator(self.coin.upper(), Client.KLINE_INTERVAL_1MINUTE,
+        klines = self.__client.get_historical_klines_generator(self.coin.upper(), Client.KLINE_INTERVAL_1MINUTE,
                                                                  timeFrame[f'{tf}'])
         for i in klines:
             temp.append((float(i[2]) + float(i[3])) / 2)
         return round(sum((float(temp[i]) for i in range(0, int(len(temp))))) / int(len(temp)),4)
 
-    def _formula(self, price_coin):
+    def __formula(self, price_coin):
         price_coin = float(price_coin)
         return price_coin*(self.profit + 0.25)/100+price_coin
 
     def check_orders(self):
-        self._client.get_all_orders()
+        self.__client.get_all_orders()
 
     def create_order_buy(self, telegram_id):
         price = self.get_coin_average('1m')
         buy_coin = math.floor(self.usdt/price)
         price_str = '{:0.0{}f}'.format(price, 4)
         spot_database(telegram_id).createNewOrder(buy_coin, self.coin, self.priceCoin, 'BUY')
-        self._client.create_order(
+        self.__client.create_order(
             symbol=self.coin,
             side='BUY',
             type='LIMIT',
@@ -54,14 +55,14 @@ class BinanceClient:
     def create_order_sell(self, telegram_id):
         price_coin = spot_database(telegram_id).take_price_coin()[0]
         coin_sell = int(spot_database(telegram_id).take_price_coin()[1])
-        print(self._formula(price_coin), coin_sell)
-        self._client.create_order(
+        print(self.__formula(price_coin), coin_sell)
+        self.__client.create_order(
             symbol=self.coin,
             side='SELL',
             type='LIMIT',
             quantity=coin_sell,
             timeInForce='GTC',
-            price=round(self._formula(price_coin),4)
+            price=round(self.__formula(price_coin),4)
         )
         print('Ордер на продажу создан для - ', telegram_id)
 
